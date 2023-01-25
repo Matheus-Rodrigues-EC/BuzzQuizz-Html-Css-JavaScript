c820c7659fe9b7d760c0eaa8d3bea2e8b1eea5f4
AndreHilario <andre18hilario@gmail.com>
2023-01-25 10:54:43 -0300

    Versão com funcionalidade do botão criar Quizz da tela 1

---
+++ script1.js
@@ -0,0 +1,7 @@
+function criarMeuQuizz(){
+    const quizz = document.querySelector('.tela1');
+    quizz.classList.add('escondido');
+
+    const criarQuizz = document.querySelector('.tela3');
+    criarQuizz.classList.remove('escondido');
+}
