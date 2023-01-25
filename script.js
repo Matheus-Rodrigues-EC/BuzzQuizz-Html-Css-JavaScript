e0e69a302115af87b10b90ad65dbdd37a7e02302
AndreHilario <andre18hilario@gmail.com>
2023-01-25 17:03:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function criarMeuQuizz(){
     const quizz2 = document.querySelector('.tela1-com-quizz');
     quizz2.classList.add('escondido');
 }
-const tituloQuizz = "";
-const imagemQuizz = "";
-const perguntasQuizz = "";
-const nivelQuizz = "";
+let tituloQuizz = "";
+let imagemQuizz = "";
+let perguntasQuizz = "";
+let nivelQuizz = "";
 function prosseguirParaPerguntas(){
     tituloQuizz = document.querySelector('.novo-titulo-quizz').value;
     imagemQuizz = document.querySelector('.nova-imagem-quizz').value;
     perguntasQuizz = Number(document.querySelector('.nova-pergunta-quizz').value);
     nivelQuizz = Number(document.querySelector('.novo-nivel-quizz').value);
     
-    if((tituloQuizz.length >= 20 && tituloQuizz.length <= 65) || perguntasQuizz >= 3|| nivelQuizz >= 2){
+    if((tituloQuizz.length >= 20 && tituloQuizz.length <= 65) || imagemQuizz === URL || perguntasQuizz >= 3|| nivelQuizz >= 2){
         //adcionar a página para a criação das perguntas
     } else {
         alert('Por favor! Preencha os dados corretamente');
